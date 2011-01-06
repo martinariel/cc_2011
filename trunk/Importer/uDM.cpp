a20782b4c97eb96814535670d3d437bf57a097f2
mfernandez <mfernandez@a53386d6-3ae3-477d-b033-15ad6cab1b48>
2011-01-06 18:30:58 +0000

    Proyecto de importación. Avance importacion scouting.

---
+++ trunk/Importer/uDM.cpp
@@ -0,0 +1,197 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "uDM.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TDM *DM;
+//---------------------------------------------------------------------------
+__fastcall TDM::TDM(TComponent* Owner)
+	: TDataModule(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TDM::DataModuleCreate(TObject *Sender)
+{
+	DWConnection->ConnectionString = "FILE NAME=" + ChangeFileExt(Application->ExeName, ".udl" );
+	DWConnection->Open();
+
+	mediaList = new list<MediaFile*>();
+
+	iterateScouting ( "G:\\01 importacion masiva de datos\\01 scouting");
+}
+//---------------------------------------------------------------------------
+void __fastcall TDM::DataModuleDestroy(TObject *Sender)
+{
+	DWConnection->Close();
+	clearMediaList();
+	delete mediaList;
+}
+//---------------------------------------------------------------------------
+
+void TDM::connectXLS ( const AnsiString& fileName )
+{
+	if ( XLSConnection->Connected )
+		XLSConnection->Close();
+
+	XLSConnection->ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + fileName + ";Extended Properties=Excel 8.0";
+	XLSConnection->Open();
+}
+
+//---------------------------------------------------------------------------
+
+void TDM::iterateScouting ( const AnsiString& path , int level )
+{
+	TSearchRec sr;
+
+	if ( level < 4 )
+	{
+		if ( FindFirst ( path + "\\*.*", faDirectory, sr) == 0)
+		{
+			do
+			{
+				if ( sr.Name[1] == '.')
+					continue;
+
+				switch ( level )
+				{
+				case 0: anio   = sr.Name; break;
+				case 1: codigo = sr.Name; break;
+				case 2:	mes    = sr.Name; break;
+				case 3: dia    = sr.Name; break;
+				}
+				iterateScouting ( path + "\\" + sr.Name , level + 1 );
+			}
+			while (FindNext(sr) == 0);
+			FindClose(sr);
+		}	}	else	{		//1 - Busco el excel
+		if ( FindFirst ( path + "\\*.xls" , faArchive , sr )  == 0 )
+		{
+        	connectXLS( path + "\\" + sr.Name );
+		}
+
+		//2 - Cargo la lista de media
+		loadMediaList ( path );
+
+		//3 - Proceso el excel
+		loadCurrentScouting();
+    }}
+
+//---------------------------------------------------------------------------
+
+void TDM::loadMediaList ( const AnsiString& path )
+{
+	TSearchRec sr;
+	clearMediaList();
+
+	if ( FindFirst ( path + "\\*.*", faDirectory, sr) == 0)
+	{
+		do
+		{
+			if ( !isMediaFolder ( sr.Name ) )
+				continue;
+
+			TSearchRec srMedia;
+
+			if ( FindFirst ( path + "\\" + sr.Name + "\\*.*", faArchive, srMedia) == 0)
+			{
+				do
+				{
+					if ( isMediaFile ( srMedia.Name ) )
+					{
+						MediaFile* mf = new MediaFile();
+						mf->path = path + "\\" + sr.Name + "\\" + srMedia.Name;
+						mf->name = srMedia.Name;
+						mediaList->push_back( mf );
+					}
+				}
+				while ( FindNext(srMedia) == 0 );
+			}
+		}
+		while (FindNext(sr) == 0);
+		FindClose(sr);
+	}
+
+	// Terminamos de cargar la media list, ahora completamos los personcode utilizando la regex
+
+	for ( list<MediaFile*>::iterator i = mediaList->begin() ; i != mediaList->end(); ++i )
+	{
+		MediaFile* mf = *i;
+		mf->code = extractPersonCode ( mf->name );
+	}
+}
+
+//---------------------------------------------------------------------------
+
+void TDM::loadCurrentScouting ( void )
+{
+
+}
+
+//---------------------------------------------------------------------------
+
+bool TDM::isMediaFolder ( const AnsiString& name )
+{
+	UnicodeString us = LowerCase ( name );
+
+	return (
+		name[1] != '.' &&
+		(
+			Pos ( "foto "   , us ) > 0 ||
+			Pos ( "fotos "  , us ) > 0 ||
+			Pos ( "video "  , us ) > 0 ||
+			Pos ( "videos " , us ) > 0 ||
+			Pos ( "qt  "    , us ) > 0
+		)
+	);
+}
+
+//---------------------------------------------------------------------------
+
+bool TDM::isMediaFile ( const AnsiString& name )
+{
+	UnicodeString us = LowerCase ( name );
+
+	return (
+		Pos( ".jpg" , us ) > 0 ||
+		Pos( ".gif" , us ) > 0 ||
+		Pos( ".png" , us ) > 0 ||
+		Pos( ".mov" , us ) > 0 ||
+		Pos( ".mpg" , us ) > 0 ||
+		Pos( ".mpeg", us ) > 0 ||
+		Pos( ".avi" , us ) > 0
+	);
+
+}
+
+//---------------------------------------------------------------------------
+
+void TDM::clearMediaList ( void )
+{
+	for ( list<MediaFile*>::const_iterator i = mediaList->begin() ; i != mediaList->end() ; ++i )
+		delete *i;
+
+	mediaList->clear();
+}
+
+//---------------------------------------------------------------------------
+
+int TDM::extractPersonCode ( const AnsiString& fullString )
+{
+	AnsiString temp;
+
+	for ( int i = 0 ; i < fullString.Length() ; i++ )
+	{
+		char ch = fullString[i+1];
+		if ( !isdigit(ch) )
+			if ( temp.Length() > 0 )
+				break;
+			else
+				continue;
+		temp += ch;
+	}
+	return ( temp.Length() > 0 ) ? StrToInt ( temp ) : -1;
+}
---
+++ trunk/Importer/uDM.dfm
@@ -0,0 +1,21 @@
+object DM: TDM
+  OldCreateOrder = False
+  OnCreate = DataModuleCreate
+  OnDestroy = DataModuleDestroy
+  Height = 497
+  Width = 733
+  object DWConnection: TADOConnection
+    ConnectionString = 
+      'FILE NAME=C:\Users\mfernandez\Documents\CC1\CC\Importer\CCImport' +
+      'er.udl'
+    LoginPrompt = False
+    Provider = 'C:\Users\mfernandez\Documents\CC1\CC\Importer\CCImporter.udl'
+    Left = 48
+    Top = 56
+  end
+  object XLSConnection: TADOConnection
+    LoginPrompt = False
+    Left = 168
+    Top = 56
+  end
+end
