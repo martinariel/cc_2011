50bf9c313c3a47ef9aa24b1bf4d5eeabbe1eaa7f
ekohan <ekohan@a53386d6-3ae3-477d-b033-15ad6cab1b48>
2011-01-13 07:29:08 +0000

    Commit inicial aplicación web: estructura de directorios + files framework symfony

---
+++ trunk/Web/webApp/config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/home/eric/cc_2011/trunk/Web/frameworkFiles/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfPropelPlugin');
+  }
+}
