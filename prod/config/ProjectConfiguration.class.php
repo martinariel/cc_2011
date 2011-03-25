dce74cffd98467015784a936dfd1a7f49dcddb45
ekohan <ekohan@a53386d6-3ae3-477d-b033-15ad6cab1b48>
2011-03-25 15:05:57 +0000

    files server -dir config

---
+++ prod/config/ProjectConfiguration.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '/home/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfPropel15Plugin');
+    $this->enablePlugins('sfGuardPlugin');
+    $this->enablePlugins('sfJqueryReloadedPlugin');
+    $this->enablePlugins('sfAdminDashPlugin');
+    $this->enablePlugins('DbFinderPlugin');
+  }
+}
